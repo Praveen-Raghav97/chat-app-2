207d32a9619028d8bb3f256ac579b9f1549c9872
Praveen-Raghav97gir config --global user.email rishavthakur9027@gmail.com <158564215+Praveen-Raghav97@users.noreply.github.com>
2025-01-29 21:52:47 +0530

    fix: remove unnecessary responsive class from Sidebar component

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -48,7 +48,7 @@ const Sidebar = () => {
             key={user._id}
             onClick={() => setSelectedUser(user)}
             className={`
-              w-full flex md:flex-col flex-row items-center gap-3
+              w-full flex  flex-row items-center gap-3
               hover:bg-base-300 transition-colors
               ${selectedUser?._id === user._id ? "bg-base-300 ring-1 ring-base-300" : ""}
             `}
