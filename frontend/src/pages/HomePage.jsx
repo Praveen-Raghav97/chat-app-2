2a592bfcfd9d6bd6bab6dee0769cee38b23b72a1
Praveen-Raghav97gir config --global user.email rishavthakur9027@gmail.com <158564215+Praveen-Raghav97@users.noreply.github.com>
2025-01-30 19:24:59 +0530

    fix: adjust HomePage margin for improved layout on medium screens

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
   const { selectedUser } = useChatStore();
 
   return (
-    <div className="h-screen mt-4 w-full bg-base-200">
+    <div className="h-screen md:mt-4 w-full bg-base-200">
       <div className="flex items-center justify-center pt-16 md:px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl  md:h-[calc(100vh-8rem)]">
           <div className=" flex  justify-around flex-col md:flex-row  md:gap-10 md:px-8  md:py-8 md:h-full h-screen  rounded-lg ">
